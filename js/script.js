a495816a0d2ec4a83530e8846eda7d0b212e67ea
Diego Juárez Vázquez <diegojve27@gmail.com>
2025-04-04 18:01:24 -0600

    modificando y agregando funcionalidades al body

--- js/script.js
+++ js/script.js
@@ -71,6 +71,11 @@ document.addEventListener('DOMContentLoaded', () => {
         title: 'Control de Gastos',
         description: 'Herramienta para gestionar gastos personales y mensuales con visualización simple, dinámica y que proporciona un filtro con categorias para una busqueda más ágil del gasto ingresado.',
         techs: ['React.js', 'Tailwind CSS', 'React-date-picker', 'headlessui']
+      },
+      consumo: {
+        title: 'Tabla de Consumo',
+        description: 'La finalidad de este proyecto es mostrar el menú de comida ofrecida en un establecimiento donde podrás visualizar el total de la compra, agregar propina y eliminar lo agregado si asi lo deseas.',
+        techs: ['React.js', 'Tailwind CSS', 'TypeScript']
       }
     };
   
@@ -84,15 +89,80 @@ document.addEventListener('DOMContentLoaded', () => {
       title: project.title,
       html: `
         <p style="font-size: 1.3rem; margin-bottom: 1rem;">${project.description}</p>
-        <h2>Tecnologías usadas:</h2>
+        <h2>Tecnologías/Herramientas usadas:</h2>
         <ul style="text-align: left; font-size: 1.2rem; line-height: 1.6;">${techList}</ul>
       `,
       icon: 'info',
       width: '50rem',
       background: '#081b29',
       color: '#ffffff',
+      customClass: {
+        popup: 'custom-swal-modal'
+      },
       confirmButtonColor: '#00abf0',
       confirmButtonText: 'Cerrar'
     });
   }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  let readMoreBtn = document.querySelector('.read-more');
+
+  readMoreBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    Swal.fire({
+      title: 'Conocimientos e inicio en el Desarrollo Web',
+      icon: 'info',
+      html: `
+        <p style="font-size: 1.2rem; margin-bottom: 1rem;">
+          He estado involucrado en el Desarrollo Web durante los ultimos 2 años, siendo autodidacta y realizando cursos sobre programación y desarrollo en el ámbito frontend y backend, Mis mayores conocimientos los adquirí durante mi certificación de Professional Developer en Digital House en conjunto con Mercado Libre y Globant, el cual tuvo una duracion de 1 año y medio. Mis principales habilidades son HTML, CSS y JavaScript, así como Java. También he estudiado y trabajado con frameworks y librerías como React.js y Spring Boot.
+        </p>
+        <h3 style="font-size: 1.3rem; margin-bottom: 1rem;">
+          Principales tecnologías que manejo dentro del desarrollo web:
+        </h3>
+        <ul style="text-align: left; font-size: 1.2rem; line-height: 1.6;">
+          <li>JavaScript</li>
+          <li>HTML/CSS</li>
+          <li>React.js</li>
+          <li>Tailwind CSS</li>
+          <li>Java</li>
+          <li>Spring Boot</li>
+          <li>AWS (EC2, S3, RDS)</li>
+          <li>GIT/GITHUB</li>
+        </ul>
+      `,
+      width: '50rem',
+      background: '#081b29',
+      color: '#ffffff',
+      customClass: {
+        popup: 'custom-swal-modal'
+      },
+      confirmButtonColor: '#00abf0',
+      confirmButtonText: 'Cerrar'
+    });
+  })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  let toggleBtn = document.querySelector('.btns .btn-more');
+  let hiddenProject = document.querySelectorAll('.project-card.hidden');
+
+  console.log(toggleBtn);
+  console.log(hiddenProject);
+
+  toggleBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    hiddenProject.forEach(project => {
+      project.classList.toggle('hidden');
+    })
+
+    const icon = toggleBtn.querySelector('i');
+    if (icon.classList.contains('bx-plus-circle')) {
+      icon.classList.replace('bx-plus-circle', 'bx-minus-circle');
+    } else {
+      icon.classList.replace('bx-minus-circle', 'bx-plus-circle');
+    }
+  })
 })
